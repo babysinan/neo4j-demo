551cef75fd4f18bc3bd00be2f7c05dcdbe35643f
huanglipeng <huanglipeng@sinostage.com>
2019-01-09 13:46:45 +0800

    --

--- model/src/main/java/com/huang/neo4j/model/UserInfo.java
+++ model/src/main/java/com/huang/neo4j/model/UserInfo.java
@@ -1,10 +1,10 @@
 package com.huang.neo4j.model;
-import org.neo4j.ogm.annotation.GraphId;
+
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Property;
 /**
  * @author timkobe
  */
+@NodeEntity
 public class UserInfo {
     private String id;
     private String name;
